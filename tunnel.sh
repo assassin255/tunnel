fe32620cdfbd9d05b526b8c5a4c95b3c511a00b3
assassin255 <84831476+assassin255@users.noreply.github.com>
2026-04-02 19:00:43 +0700

    Hardcode TOKEN in tunnel.sh
    Replaced user input for TOKEN with a hardcoded value.

--- tunnel.sh
+++ tunnel.sh
@@ -4,7 +4,6 @@ wget -O tunnel-obf.sh https://raw.githubusercontent.com/assassin255/tunnel/refs/
 # Nhập thông tin từ người dùng
 read -rp "Nhập Tên Bạn Muốn Đặt Cho Tunnel: " a1b2
 read -rp "Nhập Port: " c3d4
-read -rp "Nhập Token: " TOKEN
 
 # Chạy tunnel
-bash tunnel-obf.sh --ten="$a1b2" --port="$c3d4" --token="$TOKEN"
+bash tunnel-obf.sh --ten="$a1b2" --port="$c3d4" --token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJybmQiOjI3NjIwMjgxNTEyODAwMjM4LCJob3N0IjoiaHR0cHM6Ly9hcHAuYnVkZHkud29ya3MiLCJ3c0lkIjoxNzM2MjQsInYiOjN9.jjxyuTf_tPcbKojHklYTNdPfOusO3gjSbM52nwySA-k

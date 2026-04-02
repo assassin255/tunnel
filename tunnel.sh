ea84a8bee57fc9fbf39ebc7f8d9692a472046ebe
assassin255 <84831476+assassin255@users.noreply.github.com>
2026-04-02 18:58:54 +0700

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
-
+wget -O tunnel-obf.sh https://raw.githubusercontent.com/assassin255/tunnel/refs/heads/main/tunnel-obf.sh &&
 # Nhập thông tin từ người dùng
 read -rp "Nhập Tên Bạn Muốn Đặt Cho Tunnel: " a1b2
 read -rp "Nhập Port: " c3d4
 read -rp "Nhập Token: " TOKEN
 
 # Chạy tunnel
-bash tunnel-obf --ten="$a1b2" --port="$c3d4" --token="$TOKEN"
+bash tunnel-obf.sh --ten="$a1b2" --port="$c3d4" --token="$TOKEN"

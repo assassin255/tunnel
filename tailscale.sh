3ca8a4a80269f251250711bcf00f601d61edac68
assassin255 <84831476+assassin255@users.noreply.github.com>
2026-04-03 09:23:30 +0700

    Create tailscale.sh

---
+++ tailscale.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://tailscale.com/install.sh | sh && tailscaled --tun=userspace-networking & sleep 2 && tailscale up --authkey tskey-auth-kJwfTx1fgo11CNTRL-VTdEZ1mopbjYCPjCVoJjbjtqjR1NGrz4
